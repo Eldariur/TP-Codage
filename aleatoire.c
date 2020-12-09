9e0561de09bdca5e471265ded7ec54cb86e792fe
Gaiall <nathan.manson.72@gmail.com>
2020-12-09 17:50:10 +0100

    toujours plus de changements de FOU furieux

--- aleatoire.c
+++ aleatoire.c
@@ -5,159 +5,161 @@
 #include <math.h>
 #include "pow.h"
 #include "aleatoire.h"
+#include "recepteur.h"
 
 
 void M(char *s){
-  printf("==============MARQUEUR %s=================\n",s);
+   printf("==============MARQUEUR %s=================\n",s);
 }
+
 void P(int n){
-  for(int i=0;i<n;i++){
-    puts("");
-  }
+   for(int i=0;i<n;i++){
+      puts("");
+   }
 }
 
 // programme copié de hadamar.c
 int taille_mat2(int nb_users){
-  int c=0;/*compteur*/
-  int p=0; /*puissance de 2 */
-  int taille=pow1(2,p); /*dimension de la matrice d'hadamard*/
+   int c=0;/*compteur*/
+   int p=0; /*puissance de 2 */
+   int taille=pow1(2,p); /*dimension de la matrice d'hadamard*/
 
-  if(nb_users == 1)return 1;
-  else if( nb_users == 3)return 4;
-  else {
-    while(c <= nb_users){
+   if(nb_users == 1)return 1;
+   else if( nb_users == 3)return 4;
+   else {
+      while(c <= nb_users){
 
-      if(taille<c){
-        p++;
-        taille = pow1(2,p);
+         if(taille<c){
+            p++;
+            taille = pow1(2,p);
+         }
+
+         c++;
+
       }
 
-      c++;
-
-    }
-
-    return taille;
-  }
+      return taille;
+   }
 }
 
 
 
 /* But: Mettre en oeuvre un codeur Gold et JPL pour générer une séquence binaire aléatoire */
 void mainAleat(){
-  /* ========== DECLARATION ============= */
-  int taille_seq = 20; // ex: taille de 20 à générer
-  int long_mat = 2; // long par défaut
-  int i,j,y,z,n=0;
+   /* ========== DECLARATION ============= */
+   int taille_seq = 20; // ex: taille de 20 à générer
+   int long_mat = 2; // long par défaut
+   int i,j,y,z,n=0;
 
-  char car[20];
-  char car2;
-  int truc;
-  int continu = 1;
+   char car[20];
+   char car2;
+   int truc;
+   int continu = 1;
 
 
-  /* ========== Initialisation ============= */
-  /*
-      Pour avoir une séquence de longueur n (ex: 20) on choisi de ne garder que les n premiers bits
-      de la séquence générée au lieu de prendre aléatoirement une séquence de n bits dedans
-      cela nous évite de garder en mémoire un tableau de taille 3*7*31 par exemple
-      et également d'avoir une taille fixe de tableau
-  */
-  puts( " ====================== DEBUT DE PROGRAMME ========================= \n" ) ;
-  printf("Quel est la longueur de la séquence à générée : \n");
-  scanf("%d", &taille_seq);
-  /* variables */
-  int long_seq = taille_seq;
-  char seq_gene[long_seq];
-  char poly_gene[20];// ex: polynôme générateur 01001
+   /* ========== Initialisation ============= */
+   /*
+   Pour avoir une séquence de longueur n (ex: 20) on choisi de ne garder que les n premiers bits
+   de la séquence générée au lieu de prendre aléatoirement une séquence de n bits dedans
+   cela nous évite de garder en mémoire un tableau de taille 3*7*31 par exemple
+   et également d'avoir une taille fixe de tableau
+   */
+   puts( " ====================== DEBUT DE PROGRAMME ========================= \n" ) ;
+   printf("Quel est la longueur de la séquence à générée : \n");
+   scanf("%d", &taille_seq);
+   /* variables */
+   int long_seq = taille_seq;
+   char seq_gene[long_seq];
+   char poly_gene[20];// ex: polynôme générateur 01001
 
-  for (i = 0; i < long_seq; i++) {
-    seq_gene[i]='0';
-  }
+   for (i = 0; i < long_seq; i++) {
+      seq_gene[i]='0';
+   }
 
-//  while(car != 'N'){
-//puts("\nSouhaitez-vous continuer ?[Y:N]");
+   //  while(car != 'N'){
+   //puts("\nSouhaitez-vous continuer ?[Y:N]");
 
-  while(continu == 1){
-    int choix=0;
+   while(continu == 1){
+      int choix=0;
 
-    printf("Quel est le polynôme générateur (ex:01001): \n");
-    scanf("%s", poly_gene);
+      printf("Quel est le polynôme générateur (ex:01001): \n");
+      scanf("%s", poly_gene);
 
-    long_mat = strlen(poly_gene);
-    char seq[long_mat];
+      long_mat = strlen(poly_gene);
+      char seq[long_mat];
 
-    printf("Quel est le registre d'initialisation: \n");
-    scanf("%s", seq);
-    printf("longueur de taille : %d à générer\n",long_mat);
+      printf("Quel est le registre d'initialisation: \n");
+      scanf("%s", seq);
+      printf("longueur de taille : %d à générer\n",long_mat);
 
-    // /* ======================= */
+      // /* ======================= */
 
-    //test affichage
-    puts("tab générateur: ");
-    for(i=0;i<long_mat;i++){
-      printf("%c |",poly_gene[i]);
-    }
-    P(2);
-    //tempo = 01001
-    //seq   = 11111
+      //test affichage
+      puts("tab générateur: ");
+      for(i=0;i<long_mat;i++){
+         printf("%c |",poly_gene[i]);
+      }
+      P(2);
+      //tempo = 01001
+      //seq   = 11111
 
-    /* boucle tant que 11111 n'est pas réapparu */
-    for(i=0;i<long_seq;i++){ //long_seq-1
-        n=0;
-        printf("seq_gene[%d]=%d + %d \n",i,seq[long_mat-1]-'0',seq_gene[i]-'0');
-        seq_gene[i]=( ((seq[long_mat-1]-'0') + (seq_gene[i]-'0')) %2)+'0';
-        //test affichage
-        P(1);
-        for(z=0;z<long_mat;z++){
-          printf("%c|",seq[z]);
-        }
-        P(1);
-        for(y=long_mat-1;y!=-1;y--){ //on boucle à l'envers pour ne pas regarder ce qu'on vient de modifier
-          if(poly_gene[y] == '1'){
+      /* boucle tant que 11111 n'est pas réapparu */
+      for(i=0;i<long_seq;i++){ //long_seq-1
+         n=0;
+         printf("seq_gene[%d]=%d + %d \n",i,seq[long_mat-1]-'0',seq_gene[i]-'0');
+         seq_gene[i]=( ((seq[long_mat-1]-'0') + (seq_gene[i]-'0')) %2)+'0';
+         //test affichage
+         P(1);
+         for(z=0;z<long_mat;z++){
+            printf("%c|",seq[z]);
+         }
+         P(1);
+         for(y=long_mat-1;y!=-1;y--){ //on boucle à l'envers pour ne pas regarder ce qu'on vient de modifier
+         if(poly_gene[y] == '1'){
             n+=seq[y]-'0';
-          }
-          if(y == 0){
+         }
+         if(y == 0){
             //on ne fait rien
-          }
-          else{
+         }
+         else{
             seq[y]=seq[y-1];
-          }
-        }
-        //printf("n=%d\n",n );
-        n=n%2;
-        seq[0]=n+'0';
+         }
+      }
+      //printf("n=%d\n",n );
+      n=n%2;
+      seq[0]=n+'0';
 
-        //printf("le bit sera : %d\n",n );
+      //printf("le bit sera : %d\n",n );
 
-    }// fin boucle principale
+   }// fin boucle principale
 
-    puts("\nLa séquence générée est :\n");
-    for (i = 0; i < long_seq; i++) {
+   puts("\nLa séquence générée est :\n");
+   for (i = 0; i < long_seq; i++) {
       printf("%c|",seq_gene[i] );
-    }
+   }
 
-    puts("\nVoulez vous continuer ? [Y:N]");
-    scanf("%s",&car2 );
+   puts("\nVoulez vous continuer ? [Y:N]");
+   scanf("%s",&car2 );
 
-    while(choix == 0 && continu == 1){
+   while(choix == 0 && continu == 1){
       switch(car2){
-        case 'Y':
-          choix=1;
-          break;
-        case 'N':
-          choix=1;
-          continu=0;
-          break;
-        default :
-          puts("\nCaractère non reconnu: [Y:N]");
-          scanf("%s",&car2 );
-          break;
+         case 'Y':
+         choix=1;
+         break;
+         case 'N':
+         choix=1;
+         continu=0;
+         break;
+         default :
+         puts("\nCaractère non reconnu: [Y:N]");
+         scanf("%s",&car2 );
+         break;
       }//fin switch
-    }
+   }
 
 }//fin while
 
 
 
-  puts( "\n====================== FIN DE PROGRAMME ========================= \n" ) ;
+puts( "\n====================== FIN DE PROGRAMME ========================= \n" ) ;
 }
--- main.c
+++ main.c
@@ -8,24 +8,11 @@
 #include "affichage.h"
 #include "recepteur.h"
 #include "canal.h"
+#include "aleatoire.h"
 
 
 int main(){
-	/* ========== DECLARATION ============= */
-	int nb_utilisateur = 4;
-	int taille = 4;
-	int ** mat_hadamard;
-	int ** codes_utilisateur;
-	int num_sequence=0;
-	int N = 3;
-
-	srand(time(NULL));
-
-
-	/* ======================= */
-
-	/* ========== Initialisation ============= */
-	puts( " ====================== DEBUT DE PROGRAMME ========================= \n" ) ;
+		puts( " ====================== DEBUT DE PROGRAMME ========================= \n" ) ;
 
 	printf("Code a éxecuter :\n");
 	printf("1 - Code d'étalement\n");
@@ -38,6 +25,8 @@ int main(){
 	} while(choix < 1 || choix > 4);
 
 	switch(choix){
+		case 1: testHadamard();
+			exit(EXIT_SUCCESS);
 		case 2: testHDBN();
 			exit(EXIT_SUCCESS);
 		case 3: mainAleat();
@@ -46,128 +35,6 @@ int main(){
 			printf("WIP\n");
 			exit(EXIT_SUCCESS);
 	}
-
-	printf("Veuillez rentrer le nombre d'utilisateurs : ");
-
-	scanf("%d",&nb_utilisateur);
-	if(nb_utilisateur == -1){
-		testHDBN();
-		exit(EXIT_SUCCESS);
-	}
-	taille = taille_mat_selon_user(nb_utilisateur);
-
-	/* ======================= */
-
-	//mot envoyé par l'utilisateur
-	int mot[N];
-	//tableau contenant la sequence codée
-	int sequence[taille];
-	//tableau contenant le mot codé
-	int mot_code[taille*N];
-
-	//Géneration de la séquence aléatoire
-	for(int i=0;i<N;i++){
-		mot[i] = rand()%2;
-	}
-
-	printf("La séquence générée : ");
-	for(int i=0;i<N;i++){
-		printf(" %d ",mot[i]);
-	}
-	printf("\n");
-
-	/* ========== ALLOCATION ============= */
-	mat_hadamard = (int **) malloc(taille * sizeof(int *));
-	if(NULL == mat_hadamard){
-		perror("Pb malloc sur la matrice hadamard");
-		exit(EXIT_FAILURE);
-	}
-
-	for(int i = 0; i < taille; ++i){
-		mat_hadamard[i] = (int *) malloc(taille * sizeof(int));
-		if(NULL == mat_hadamard[i]){
-			perror("Pb malloc sur une ligne de la matrice hadamard");
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	codes_utilisateur = malloc(nb_utilisateur * sizeof(int **));
-	if(NULL == mat_hadamard){
-		perror("Pb malloc sur le tableau contenant les codes des utilisateurs");
-		exit(EXIT_FAILURE);
-	}
-
-	for(int i = 0; i < nb_utilisateur; ++i){
-		codes_utilisateur[i] = malloc(N * taille * sizeof(int));
-		if(NULL == mat_hadamard[i]){
-			perror("Pb malloc sur une ligne du tableau contenant les codes des utilisateurs");
-			exit(EXIT_FAILURE);
-		}
-	}
-	/* ======================= */
-
-
-	/* ========== Programme ============= */
-
-	/*Création et affichage de la matrice de Hadamard de taille correspondante*/
-	init_mat(mat_hadamard,taille);
-	generer_hadamard(mat_hadamard,taille);
-	printf("\nLa matrice Hadamard de taille %d est :\n", taille);
-	afficher_hadamard(mat_hadamard,taille);
-
-	/*Attribution d'une sequence pour chaque user*/
-	for(int utilisateur = 0; utilisateur < nb_utilisateur; utilisateur++){
-
-		/*selection d'une ligne libre au hasard*/
-		if((num_sequence = selecteur(taille)) == -1){
-			perror("probleme selection ligne dans matrice hadamard");
-			exit(EXIT_FAILURE);
-		}
-
-		for(int i = 0; i < taille; i++){
-			sequence[i]= mat_hadamard[num_sequence][i];
-		}
-		printf("Le user %d utilise la séquence n°%d : \n", utilisateur+1, num_sequence+1);
-		afficher_tab_int(sequence, taille);
-
-		/*Temporisation pour un meilleur affichage*/
-		sleep(1);
-
-		/*boucle de chiffrage du mot*/
-		for(int i = 0; i < N; i++) {
-			/*On repete N fois la sequence en appliquant le mot dessus*/
-			for(int j = i*taille; j < (i * taille + taille); j++) {
-				if(mot[i]){
-					mot_code[j] = sequence[j % taille] == 1 ? 1 : 0;
-				} else {
-					mot_code[j] = -1 * sequence[j % taille] == 1 ? 1 : 0;
-				}
-			}
-		}
-
-		/*on ajoute le mot codé au tableau contenant tout les mots codés*/
-		for(int i = 0; i < taille * N; i++)
-			codes_utilisateur[utilisateur][i] = mot_code[i];
-
-		printf("Voici la sequence codée : ");
-		afficher_tab_int(mot_code, taille*N);
-
-		afficher_separateur();
-
-      recepteur(mot_code,sequence,taille,N);
-	}
-
-	/* ======================= */
-
-	/* ========== LIBERATION ============= */
-	for (int i = 0; i < taille; ++i)
-	free(mat_hadamard[i]);
-	free(mat_hadamard);
-	for (int i = 0; i < nb_utilisateur; ++i)
-	free(codes_utilisateur[i]);
-	free(codes_utilisateur);
-	/* ======================= */
-
 
 	puts( "\n ====================== FIN DE PROGRAMME ========================= \n" ) ;
 
